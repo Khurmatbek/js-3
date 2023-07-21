3e5115f328968e85b7f42382517ae57453da6f27
Khurmatbek <xurmatbekolloyorov1514@gmail.com>
2023-07-21 14:25:29 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// summ uchun 
+var airPlaneUSD = 500;
+var hotelUSD = 250;
+var museumYEVRO = 120;
+var SUMM;
+USD = SUMM * 11590;
+YEVRO = SUMM * 12450;
+result = airPlaneUSD * USD + hotelUSD * USD + museumYEVRO * YEVRO;
+confirm("Alisher pulingiz somda bolsa iashlaymiz:")
+let alishersumm =prompt("how much money do you have(SUMM:)?");
+console.log(`Alisherning puli(summda):${alishersumm}`);
+if (Number.alishersumm > result )
+{
+console.log("Oq yol Alisher");
+}
+else{
+console.log("Alisher , Sabr qilish kerak");
+}
+
+
+
+
