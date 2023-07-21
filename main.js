7473307e09814c0d0baf30df0acd13ed9ece7fc6
Khurmatbek <xurmatbekolloyorov1514@gmail.com>
2023-07-22 00:26:47 +0500

    add new change

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 var airPlaneUSD = 500;
 var hotelUSD = 250;
 var museumYEVRO = 120;
-var SUMM;
+var SUMM =1;
 USD = SUMM * 11590;
 YEVRO = SUMM * 12450;
 result = airPlaneUSD * USD + hotelUSD * USD + museumYEVRO * YEVRO;
 confirm("Alisher pulingiz somda bolsa iashlaymiz:")
 let alishersumm =prompt("how much money do you have(SUMM:)?");
 console.log(`Alisherning puli(summda):${alishersumm}`);
-if (Number.alishersumm > result )
+if (Number(alishersumm) > result )
 {
 console.log("Oq yol Alisher");
 }
